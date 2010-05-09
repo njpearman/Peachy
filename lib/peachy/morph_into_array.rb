a85312bf3f765d39a035844337d76ca0b8def5c6
Neill Pearman <neill@stackulous.com>
2010-05-09 16:21:51 +0100

    Changed messages.

--- lib/peachy/morph_into_array.rb
+++ lib/peachy/morph_into_array.rb
@@ -7,7 +7,7 @@ module Peachy
     def mimic object_to_mimic
       eval_on_singleton_class do
         define_method(:method_missing) do |method_name, *args|
-          puts "[Peachy::Proxy] I can only do '#{method_name}' with '#{args}' if an array can.  You see, really, I'm an array now."
+          puts "[Peachy::Proxy] I can only do '#{method_name}' with '#{args}' if an Array can.  You see, really, I'm an array now."
           return object_to_mimic.send(method_name, *args) &block if block_given?
           return object_to_mimic.send(method_name, *args)
         end
@@ -16,7 +16,7 @@ module Peachy
 
     def morph_into_array
         raise AlreadyASingleChild.new(nokogiri_node.name) if @acts_as == :single_child
-        puts "[Peachy::Proxy] So I should be an array, then."
+        puts "[Peachy::Proxy] So I should be an Array, then."
         real_proxy = Peachy::Proxy.new nokogiri_node
         mimic [real_proxy]
         return real_proxy
