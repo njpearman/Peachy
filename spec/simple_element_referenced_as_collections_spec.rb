404e447eca40eb13a8e401f8655953c50ace1e0d
Neill Pearman <neill@stackulous.com>
2010-05-09 18:01:34 +0100

    Added spec for a simple node that is referenced as the first and only in a collection.  Tests don't pass, though, as this has introduced the need to make some fundamental design choices.

--- spec/simple_element_referenced_as_collections_spec.rb
+++ spec/simple_element_referenced_as_collections_spec.rb
@@ -1,9 +1,9 @@
-describe "a childless element with attributes referenced as the first part of a collection" do
+describe "a simple element referenced as the first part of a collection" do
   before(:each) do
     xml = <<XML
 <xml>
   <list>
-    <item id="1">Hello</item>
+    <item>Hello</item>
   </list>
 </xml>
 XML
@@ -11,11 +11,7 @@ XML
   end
 
   it "should infer that the element matches the first index of an array" do
-    @proxy.xml.list.item[0].value.should == 'Hello'
-  end
-
-  it "should make attributes available on the single child" do
-    @proxy.xml.list.item[0].id.should == '1'
+    @proxy.xml.list.item[0].should == 'Hello'
   end
 
   it "should quack like an array" do
@@ -30,15 +26,7 @@ XML
   end
 
   it "should raise an error if the element value has already been accessed as an only child" do
-    @proxy.xml.list.item.value
-    lambda { @proxy.xml.list.item[0] }.should raise_error AlreadyASingleChild, <<MSG
-The 'item' node has already been accessed as a single child, but you are now trying to use it as a collection.
-Do not try to access Peachy::Proxies in a mixed manner in your implementation.
-MSG
-  end
-
-  it "should raise an error if the element's attributes have already been accessed as an only child" do
-    @proxy.xml.list.item.id
+    @proxy.xml.list.item
     lambda { @proxy.xml.list.item[0] }.should raise_error AlreadyASingleChild, <<MSG
 The 'item' node has already been accessed as a single child, but you are now trying to use it as a collection.
 Do not try to access Peachy::Proxies in a mixed manner in your implementation.
