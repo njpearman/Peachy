57dbab4563128dc5da4234346e8d71c13da182ee
unknown <neill@.7digital.local>
2010-05-27 14:19:48 +0100

    Added spec for mimic, and removed bug that could result in an infinite loop in Mimic.

--- lib/peachy/mimic.rb
+++ lib/peachy/mimic.rb
@@ -1,5 +1,12 @@
+class NothingToMimic < Exception
+end
+
 module Peachy
   module Mimic
+    def mimic
+      raise NothingToMimic.new
+    end
+    
     def method_missing method_name, *args, &block
       mimic.send(method_name, *args, &block)
     end
