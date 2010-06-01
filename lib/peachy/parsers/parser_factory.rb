01ad3747e55b1475f8839188327e708792e190e2
Neill Pearman <neill@stackulous.com>
2010-06-01 08:47:06 +0100

    Added docs for ParserFactory.

--- lib/peachy/parsers/parser_factory.rb
+++ lib/peachy/parsers/parser_factory.rb
@@ -1,6 +1,14 @@
 module Peachy
   module Parsers
     class ParserFactory
+      # Loads the first available XML parser defined in the list of parsers that
+      # Peachy can use.  This requires the Gem for the parser and creates the method
+      # make_from, which will return a new instance of the ParserWrapper for that
+      # library.
+      # In order of preference, the Gems that Peachy can currently use for parsing
+      # XML are:
+      #   * Nokogiri
+      #   * REXML
       def load_parser
         preferred_parser = Parsers.find {|parser| Gem.available? parser[:gem] }
         self.class.class_eval do
