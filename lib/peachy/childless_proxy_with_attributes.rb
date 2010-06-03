c7cc57974b486f9e916a8cf12f8b4900f11f064b
Neill Pearman <neill@stackulous.com>
2010-06-04 00:41:54 +0100

    Updated docs.

--- lib/peachy/childless_proxy_with_attributes.rb
+++ lib/peachy/childless_proxy_with_attributes.rb
@@ -7,6 +7,8 @@ module Peachy
     end
 
     private
+    # The implementation for Proxy#generate_method_for_xml is far more straightforward
+    # when the underlying element is a childless element that has attributes.
     def generate_method_for_xml method_name
       method_name.check_for_convention
       Peachy::Proxy::CurrentMethodCall.new(self, method_name).create_method_for_attribute {|match| no_matching_xml(method_name) if match.nil? }
