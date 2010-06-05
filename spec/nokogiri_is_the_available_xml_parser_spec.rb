19e1aaecf74d50477ddaba1cfcf3b4507ace9c28
Neill Pearman <neill@stackulous.com>
2010-06-05 12:32:42 +0100

    Added guard clauses to specs for when Nokogiri is not installed.

--- spec/nokogiri_is_the_available_xml_parser_spec.rb
+++ spec/nokogiri_is_the_available_xml_parser_spec.rb
@@ -1,44 +1,48 @@
 require 'spec_helper'
-require 'nokogiri'
 
-describe "nokogiri is the available XML parser" do
-  before(:each) do
-    @factory = Peachy::Parsers::ParserFactory.new
-    expectation = @factory.stubs(:require).with('nokogiri').returns(true)
-  end
 
-  it "should indicate that the parser has been loaded" do
-    @factory.load_parser.should be_true
-  end
+# this is to ignore this spec if the nokogiri gem is not available
+if Gem.available? 'nokogiri'
+  require 'nokogiri'
 
-  it "should check whether Nokogiri is available" do
-    expectation = Gem.expects(:available?).with('nokogiri').returns(true)
-    parser_type = @factory.load_parser
-    expectation.satisfied?.should be_true
-  end
-  
-  it "should load Nokogiri" do
-    expectation = @factory.expects(:require).with('nokogiri').returns(true)
-    parser_type = @factory.load_parser
-    expectation.satisfied?.should be_true
-  end
+  describe "nokogiri is the available XML parser" do
+    before(:each) do
+      @factory = Peachy::Parsers::ParserFactory.new
+      expectation = @factory.stubs(:require).with('nokogiri').returns(true)
+    end
 
-  it "should not load REXML" do
-    expectation = @factory.expects(:require).with('rexml/document').never.returns(true)
-    parser_type = @factory.load_parser
-    expectation.satisfied?.should be_true
-  end
+    it "should indicate that the parser has been loaded" do
+      @factory.load_parser.should be_true
+    end
 
-  it "should enable a way to a new NokogiriWrapper" do
-    @factory.load_parser
-    @factory.methods.should include('make_from')
-  end
+    it "should check whether Nokogiri is available" do
+      expectation = Gem.expects(:available?).with('nokogiri').returns(true)
+      parser_type = @factory.load_parser
+      expectation.satisfied?.should be_true
+    end
 
-  it "should create a NokogiriWrapper from xml" do
-    @factory.load_parser
-    wrapper = @factory.make_from '<thing>Stuff</thing>'
-    wrapper.should be_a(Peachy::Parsers::NokogiriWrapper)
-    wrapper.content.should == 'Stuff'
+    it "should load Nokogiri" do
+      expectation = @factory.expects(:require).with('nokogiri').returns(true)
+      parser_type = @factory.load_parser
+      expectation.satisfied?.should be_true
+    end
+
+    it "should not load REXML" do
+      expectation = @factory.expects(:require).with('rexml/document').never.returns(true)
+      parser_type = @factory.load_parser
+      expectation.satisfied?.should be_true
+    end
+
+    it "should enable a way to a new NokogiriWrapper" do
+      @factory.load_parser
+      @factory.methods.should include('make_from')
+    end
+
+    it "should create a NokogiriWrapper from xml" do
+      @factory.load_parser
+      wrapper = @factory.make_from '<thing>Stuff</thing>'
+      wrapper.should be_a(Peachy::Parsers::NokogiriWrapper)
+      wrapper.content.should == 'Stuff'
+    end
   end
 end
-
--- spec/using_peachy_proxy_incorrectly_spec.rb
+++ spec/using_peachy_proxy_incorrectly_spec.rb
@@ -4,7 +4,8 @@ describe "using Peachy::Proxy incorrectly" do
   end
 
   it 'should raise a well written error if the method does not map to anything in the underlying XML node' do
-    lambda { @proxy.missing }.should raise_error(NoMatchingXmlPart, 'missing is not contained as a child of the node document.')
+    root_name = Gem.available?('nokogiri') ? 'document' : ''
+    lambda { @proxy.missing }.should raise_error(NoMatchingXmlPart, "missing is not contained as a child of the node #{root_name}.")
   end
 
   it "should raise an error if the method name is not in Ruby convention" do
